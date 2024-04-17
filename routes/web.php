1ee3108aba70e2271fdd1f68ce119612f7c14462
kosuke.katsuno <tech24-1@78it.com>
2024-04-17 06:51:38 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\TasksController;
+
+Route::resource('tasks', TasksController::class);
+
+// トップページをタスク一覧に設定
+Route::redirect('/', '/tasks');
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
